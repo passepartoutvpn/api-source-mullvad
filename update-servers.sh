ab240b3d780a31e0cf9011ecade576328ec0fe10
Davide De Rosa <keeshux@gmail.com>
2019-10-23 19:08:07 +0200

    Comment that certs are static

--- update-servers.sh
+++ update-servers.sh
@@ -3,6 +3,10 @@ URL="https://api.mullvad.net/public/relays/v1/"
 TPL="template"
 SERVERS="$TPL/servers.json"
 
+echo
+echo "WARNING: Certs must be updated manually!"
+echo
+
 mkdir -p $TPL
 if ! curl -L $URL >$SERVERS; then
     exit
