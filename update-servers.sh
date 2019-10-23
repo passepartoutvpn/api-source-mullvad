08229bab8fff2a6869fee0a2ec5a0b21cca7205e
Davide De Rosa <keeshux@gmail.com>
2019-10-23 18:29:10 +0200

    Download servers JSON from web service

--- update-servers.sh
+++ update-servers.sh
@@ -1,43 +1,9 @@
 #!/bin/bash
-URL="https://mullvad.net/en/download/config/" # not stateless
+URL="https://api.mullvad.net/public/relays/v1/"
 TPL="template"
-STATIC="static"
-TMP="tmp"
-SERVERS_SRC="$STATIC/mullvad_config_ios_all.zip"
-SERVERS_DST="$TPL/servers.csv"
-SAMPLE_CFG="$TMP/mullvad_at.ovpn"
-CA="$TPL/ca.crt"
-
-LINES=100000
-CA_BEGIN="<ca>"
-CA_END="</ca>"
-UDP_OTHER_PORTS="53"
-TCP_PORTS="80-443"
-
-echo
-echo "WARNING: Profiles must be updated manually!"
-echo
+SERVERS="$TPL/servers.json"
 
 mkdir -p $TPL
-#if ! curl -L $URL >$SERVERS_SRC.tmp; then
-#    exit
-#fi
-#mv $SERVERS_SRC.tmp $SERVERS_SRC
-
-rm -rf $TMP
-unzip $SERVERS_SRC -d $TMP
-mv $TMP/mullvad_config_ios_all/* $TMP
-rmdir $TMP/mullvad_config_ios_all
-
-grep -A$LINES $CA_BEGIN $SAMPLE_CFG | grep -B$LINES $CA_END | egrep -v "$CA_BEGIN|$CA_END" >$CA
-
-rm -f $SERVERS_DST
-for CFG in `cd $TMP && ls *.ovpn`; do
-    ID=`echo $CFG | sed -E "s/^mullvad_([A-Za-z\-]+)\.ovpn$/\1/"`
-    ID_COMPS=(${ID//-/ })
-    COUNTRY=${ID_COMPS[0]}
-    AREA=${ID_COMPS[1]}
-    HOST=$ID.mullvad.net
-    UDP_PORT=`grep -E "^remote " $TMP/$CFG | sed -E "s/^.* ([0-9]+)$/\1/"`
-    echo $ID,$COUNTRY,$AREA,$HOST,$UDP_PORT-$UDP_OTHER_PORTS,$TCP_PORTS >>$SERVERS_DST
-done
+if ! curl -L $URL >$SERVERS; then
+    exit
+fi
