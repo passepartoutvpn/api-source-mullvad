18fdc62b93856d6fb0b5bd1d3c0f568e148d663f
Davide De Rosa <keeshux@gmail.com>
2022-03-25 09:34:46 +0100

    Upgrade to v5

--- scripts/net.rb
+++ scripts/net.rb
@@ -7,107 +7,102 @@ Dir.chdir(cwd)
 
 ###
 
-servers = File.read("../template/servers.json")
+template = File.read("../template/servers.json")
 ca = File.read("../static/ca.crt")
 
 cfg = {
-    ca: ca,
-    cipher: "AES-256-CBC",
-    auth: "SHA1",
-    frame: 0,
-    ping: 10,
-    pingTimeout: 60,
-    reneg: 0,
-    eku: true
+  ca: ca,
+  cipher: "AES-256-CBC",
+  digest: "SHA1",
+  compressionFraming: 0,
+  keepAliveSeconds: 10,
+  keepAliveTimeoutSeconds: 60,
+  renegotiatesAfterSeconds: 0,
+  checksEKU: true
 }
 
-external = {
-    hostname: "${id}.mullvad.net"
-}
-
-recommended_cfg = cfg.dup
-recommended_cfg["ep"] = [
-    "UDP:1194",
-    "UDP:1195",
-    "UDP:1196",
-    "UDP:1197",
-    "UDP:1301",
-    "UDP:1302",
-    "UDP:53",
-    "TCP:443",
-    "TCP:80"
-]
 recommended = {
-    id: "default",
-    name: "Default",
-    comment: "256-bit encryption",
-    cfg: recommended_cfg,
-    external: external
+  id: "default",
+  name: "Default",
+  comment: "256-bit encryption",
+  ovpn: {
+    cfg: cfg,
+    endpoints: [
+      "UDP:1194",
+      "UDP:1195",
+      "UDP:1196",
+      "UDP:1197",
+      "UDP:1301",
+      "UDP:1302",
+      "UDP:53",
+      "TCP:443",
+      "TCP:80"
+    ]
+  }
 }
 
-dns_override_cfg = cfg.dup
-dns_override_cfg["ep"] = [
-    "UDP:1400",
-    "TCP:1401"
-]
 dns_override = {
-    id: "dns",
-    name: "Custom DNS",
-    comment: "256-bit encryption",
-    cfg: dns_override_cfg,
-    external: external
+  id: "dns",
+  name: "Custom DNS",
+  comment: "256-bit encryption",
+  ovpn: {
+    cfg: cfg,
+    endpoints: [
+      "UDP:1400",
+      "TCP:1401"
+    ]
+  }
 }
 
 presets = [
-    recommended,
-    dns_override
+  recommended,
+  dns_override
 ]
 
 defaults = {
-    :username => "1234567890",
-    :pool => "us",
-    :preset => "default"
+  :username => "1234567890",
+  :country => "US"
 }
 
 ###
 
-pools = []
+servers = []
 
-json = JSON.parse(servers)
+json = JSON.parse(template)
 json["countries"].each { |country|
-    country["cities"].each { |city|
-        code = country["code"].upcase
-        area = city["name"]
+  country["cities"].each { |city|
+    code = country["code"].upcase
+    area = city["name"]
 
-        city["relays"].each { |relay|
-            id = relay["hostname"]
-            hostname = "#{id.downcase}.mullvad.net"
-            num = id.split("-").last.to_i
+    city["relays"].each { |relay|
+      id = relay["hostname"]
+      hostname = "#{id.downcase}.mullvad.net"
+      num = id.split("-").last.to_i
 
-            addresses = [relay["ipv4_addr_in"]]
-            addresses.map! { |a|
-                IPAddr.new(a).to_i
-            }
+      addresses = [relay["ipv4_addr_in"]]
+      addresses.map! { |a|
+        IPAddr.new(a).to_i
+      }
 
-            pool = {
-                :id => id,
-                :country => code,
-                :hostname => hostname,
-                :addrs => addresses
-            }
-            pool[:area] = area if !area.empty?
-            pool[:num] = num
-            pools << pool
-        }
+      server = {
+        :id => id,
+        :country => code,
+        :hostname => hostname,
+        :addrs => addresses
+      }
+      server[:area] = area if !area.empty?
+      server[:num] = num
+      servers << server
     }
+  }
 }
 
 ###
 
 infra = {
-    :pools => pools,
-    :presets => presets,
-    :defaults => defaults
+  :servers => servers,
+  :presets => presets,
+  :defaults => defaults
 }
 
 puts infra.to_json
